1318c7095d4c68f0ee0290003f0b2e37938cf37f
Ted Irland <irlandth@gmail.com>
2021-04-11 15:24:49 -0500

    Implemented routes that take the stream id as a param. Wired up routes to match records in the DB. NS - refactor stream creation as a reusable form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom'
 import StreamList from '../components/streams/StreamList'
 import StreamDelete from '../components/streams/StreamDelete'
 import StreamEdit from '../components/streams/StreamEdit'
 import StreamCreate from '../components/streams/StreamCreate'
 import StreamShow from '../components/streams/StreamShow'
 import Header from './Header'
+import history from '../history'
 
 const App = () => {
   return (
     <div className='ui container'>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path='/' exact component={StreamList} />
           <Route path='/streams/new' exact component={StreamCreate} />
-          <Route path='/streams/edit' exact component={StreamEdit} />
+          <Route path='/streams/edit/:id' exact component={StreamEdit} />
           <Route path='/streams/delete' exact component={StreamDelete} />
           <Route path='/streams/show' exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 }
